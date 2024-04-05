808d3bd693e6d01dca2f363d5320eae17e4802c7
ThiagoA-Menezes <assis.thiago@gmail.com>
2024-04-05 14:00:00 -0300

    Inserção do Array para download paralelo.

--- dataCleaner.py
+++ dataCleaner.py
@@ -3,6 +3,9 @@
 import urllib.request
 from tqdm import tqdm
 
+# lib importada para fazer o processamento paralelo.
+from multiprocessing import Pool
+
 def get_data_uf_ano_mes(uf ,ano, mes):
     url = f"ftp://ftp.datasus.gov.br/dissemin/publicos/SIHSUS/200801_/Dados/RD{uf}{ano}{mes}.dbc"
 
@@ -12,6 +15,7 @@ def get_data_uf_ano_mes(uf ,ano, mes):
 
 # Pega as datas e padroniza
 def get_data_uf(ufs, datas):
+    print(uf)
     for i in tqdm(datas):
         ano, mes, dia = i.split("-")
         ano = ano [-2:]
@@ -28,25 +32,32 @@ ufs  = ["AC", "AL", "AP", "AM", "BA",
 # Lista de datas que serão baixadas do site do Datasus.
 datas = ['2023-01-01']
 
-for uf in ufs:
-    print(uf)
-    get_data_uf(ufs, datas)
+#for uf in ufs:
+#    get_data_uf(ufs, datas)
 
+#data = [ ("AC", ['2023-01-01', '2023-02-01']),
+#         ("AM", ['2023-01-01', '2023-02-01']),
+#         ("AP", ['2023-01-01', '2023-02-01']),
+#          ("DF", ['2023-01-01', '2023-02-01']), ]
+# 
+#with Pool(8) as pool:
+#    pool.starmap(get_data_uf,data)
+
+# Aqui iremos colocar o array que vai fazer a baixa dos arquivos em paralelo.
+
+to_download = [(uf,datas) for uf in ufs]
+to_download
+
+
+# Python não abre o arquivo .dbc, iremos utilizar a seguinte forma para abrir os arquivos.
 
 # %%
 ## Criando um diretório para armazenamento dos dados e validando se o arquivo foi 
 ## armazenado corretamente no local indicado
 
-import os
-
-os.listdir("P:/Python - Projetos/Datasus/rd/dbc/")
-
+#import os
+#
+#os.listdir("P:/Python - Projetos/Datasus/rd/dbc/")
+#
 # Criar a pasta na pasta
 #os.mkdir("P:/Python - Projetos/Datasus/rd/dbc")
-
-# %%
-## Verificando o tamanho do arquivo
-
-
-
-#%%
